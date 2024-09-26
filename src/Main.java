bd644a0a4982643e5abfa3a9a0974c5e56ace500
adnan-bin-wahid <adnan.iit.du@gmail.com>
2024-09-26 11:48:01 +0600

    Third commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+class Car{
+    private String carID;
+    private String brand;
+    private String model;
+    private double basePricePerDay;
+    private boolean isAvailable;
+
+    public Car(String carID,String brand,String model,double basePricePerDay){
+        this.carID = carID;
+        this.brand = brand;
+        this.model = model;
+        this.basePricePerDay = basePricePerDay;
+        this.isAvailable = true;
     }
+
+    public String getCarID(){
+        return  carID;
+    }
+    public String getBrand(){
+        return brand;
+    }
+    public String getModel(){
+        return model;
+    }
+    public boolean isAvailable(){
+        return isAvailable;
+    }
+    public double calculatePrice(int rentalDays){
+        return basePricePerDay*rentalDays;
+    }
+    public void rent(){
+        isAvailable = false;
+    }
+    public void returnCar(){
+        isAvailable = true;
+    }
+}
+
+class Customer{
+
+}
+
+class Rental{
+
+}
+
+class CarRentalSystem{
+
 }
